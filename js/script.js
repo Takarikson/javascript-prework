4d746663b99f6027d6cabdbbefcddd7edc198f0c
Krzysztof Fila <kfila17@gmail.com>
2020-04-19 17:56:23 +0200

    Add PlayerMove to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+//Ruchy komputera
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
+
 if (randomNumber == 1) {
     computerMove = 'Kamień';
 } else if (randomNumber == 2) {
@@ -9,4 +11,20 @@ if (randomNumber == 1) {
 }
 printMessage('Ruch komputera to : ' + computerMove);
 console.log('1=Papier , 2=Kamień, 3=Nożyce');
-console.log('Wylosowana liczba to ' + randomNumber + ', więc jest to ' + computerMove);
+console.log('Komputer wylosował liczbę ' + randomNumber + ', więc jest/są to ' + computerMove);
+
+//Ruchy gracza
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+if (playerInput == '1') {
+    playerMove = 'Kamień';
+} else if (playerInput == '2') {
+    playerMove = 'Papier';
+} else if (playerInput == '3') {
+    playerMove = 'Nożyce';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+console.log('Twoja wpisana odpowiedź to: ' + playerInput);
