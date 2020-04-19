821744ec5f936bd4c9007dbeb95f6ab68119500e
Krzysztof Fila <kfila17@gmail.com>
2020-04-19 17:40:47 +0200

    Add ComputerMove to script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-function printMessage(msg) {
-    var div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+if (randomNumber == 1) {
+    computerMove = 'Kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'Papier';
+} else {
+    computerMove = 'Nożyce';
 }
-
-function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-}
+printMessage('Ruch komputera to : ' + computerMove);
+console.log('1=Papier , 2=Kamień, 3=Nożyce');
+console.log('Wylosowana liczba to ' + randomNumber + ', więc jest to ' + computerMove);
